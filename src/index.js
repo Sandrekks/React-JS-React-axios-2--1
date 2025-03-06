89e6d07dbad2d6057ee80ae89c22a02c20040e4f
ქეთევან გაბოძე <gabodze.ketevan@students.gov.ge>
2025-03-06 16:32:15 +0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,25 +1,40 @@
-import React, {useEffect} from "react";
+import React, {useRef, useEffect} from "react";
 import ReactDOM from "react-dom/client";
+import image from ".//images/download.jpg";
+import css from "./index.css";
+
 
 
 
 function App() {
+  const number = useRef();
 
- function useOption(a, b) {
-  console.log( a + b)
-   
+  const focusi = useRef();
 
- }
-
- useOption(40, 32)
-
-return (
-
-  console.log(40, 33)
  
-)
+    useEffect(() => {
+    number.current.focus()
+    focusi.current.focus();
+    });
+
+
+    function onClickk() {
+      window.alert("Hello World")
+    }
+    
   
+  return (
+     <>
+     <input type="text" ref={number}/>
+
+     <button onClick={onClickk} ref={focusi} >click</button>
+     
+     </>
+  )
+
 }
+
+ 
 var root = document.getElementById("root")
 ReactDOM.createRoot(root).render(<App></App>)
 
