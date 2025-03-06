b842677eaa820f456653c82afb9c5963e0ef6ee0
ქეთევან გაბოძე <gabodze.ketevan@students.gov.ge>
2025-03-06 12:59:32 +0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,28 +1,22 @@
-import React from "react";
+import React, {useEffect} from "react";
 import ReactDOM from "react-dom/client";
 
 
 
 function App() {
 
-  const num1 = 10;
-  const num2 = 34;
-  let shedegi;
-                    //ამ დავალებაში მოცემულია ორი რიცხვის შედარება ექსფრეშენში და გამოყენებულია if/else
-                      //კონსტრუქცია.
+ function useOption(a, b) {
+  console.log( a + b)
+   
 
-  if (num1 > num2) {
-    shedegi = num1;
-  } else if(num1 < num2) {
-    shedegi = num2;
-  }
+ }
+
+ useOption(40, 32)
 
 return (
-  
-  <React.Fragment>
-    <p >{shedegi}</p>
-    
-  </React.Fragment>
+
+  console.log(40, 33)
+ 
 )
   
 }
